5bf5d953c749da32e54c794f61a66d7ee09e7acb
Ldeems <luke.deems@yahoo.com>
2017-06-28 10:56:57 -0400

    works but asks 3 times

--- week_two_rev.rb
+++ week_two_rev.rb
@@ -1,10 +1,17 @@
 
 print "choose a number between 1 and 9"
 
+def choice
+	gets.chomp.to_i
+end
 
-if gets.chomp == [1,2,3,4,5,6,7,8,9].sample
+if choice == [1,2,3,4,5,6,7,8,9].sample
 	 print "you got it"
-elsif 
-	p  "nope you guessed wrong" 
+	elsif 
+	 	choice < [1,2,3,4,5,6,7,8,9].sample
+	 	print "to low"
+	elsif 
+		choice > [1,2,3,4,5,6,7,8,9].sample
+		print "to high"
 end
 	
