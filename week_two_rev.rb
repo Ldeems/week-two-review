111fbbd095178b5daf955f12322541a053180895
Ldeems <luke.deems@yahoo.com>
2017-06-28 10:36:02 -0400

    getting closer, it doesnt as twice now

--- week_two_rev.rb
+++ week_two_rev.rb
@@ -1,10 +1,10 @@
 
 print "choose a number between 1 and 9"
-p gets.chomp
 
-if gets.chomp ==	[1,2,3,4,5,6,7,8,9].sample
+
+if gets.chomp == [1,2,3,4,5,6,7,8,9].sample
 	 print "you got it"
-else
-	print "nope you guessed wrong" 
+elsif 
+	p  "nope you guessed wrong" 
 end
 	
