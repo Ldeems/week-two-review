fb67fa4a3972035db83a1fed2e792035ac86fdb4
Ldeems <luke.deems@yahoo.com>
2017-06-28 10:27:17 -0400

    trting to guess random

--- week_two_rev.rb
+++ week_two_rev.rb
@@ -1,6 +1,10 @@
 
+print "choose a number between 1 and 9"
+p gets.chomp
 
-p	[1,2,3,4,5,6,7,8,9].sample
-
-
+if gets.chomp ==	[1,2,3,4,5,6,7,8,9].sample
+	 print "you got it"
+else
+	print "nope you guessed wrong" 
+end
 	
