afa50a3bcbfd5a0d23cc1d3490c6996ba2472851
Ldeems <luke.deems@yahoo.com>
2017-06-28 11:58:42 -0400

    wooooooo it works

--- week_two_rev.rb
+++ week_two_rev.rb
@@ -1,17 +1,34 @@
 
 print "choose a number between 1 and 9"
 
-def choice
-	gets.chomp.to_i
-end
+#[1,2,3,4,5,6,7,8,9].sample == two
+#gets.chomp.to_i == one
+ 
+one = gets.chomp.to_i
+two = [1,2,3,4,5,6,7,8,9].sample
+	
 
-if choice == [1,2,3,4,5,6,7,8,9].sample
-	 print "you got it"
-	elsif 
-	 	choice < [1,2,3,4,5,6,7,8,9].sample
-	 	print "to low"
-	elsif 
-		choice > [1,2,3,4,5,6,7,8,9].sample
+if one == two
+	 print "you got it" 
+	 
+	 elsif	one < two
+	 	print "to low" 
+	
+	elsif	one > two
 		print "to high"
 end
-	
+
+
+#def blue	
+ #blue == [1,2,3,4,5,6,7,8,9].sample
+#end
+#def red	
+ #red == gets.chomp.to_i 
+#end
+
+
+#if red == blue
+#	print "you got it"
+#elsif  
+#	print "wrong"	
+#end
